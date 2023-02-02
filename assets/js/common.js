8c45a1786f4caf33429c94360d6ff545ed5b06a8
jeongsehyeon <kuro7529@gmail.com>
2023-02-03 03:11:44 +0900

    Update

--- assets/js/common.js
+++ assets/js/common.js
@@ -107,3 +107,18 @@ function typi(){
         clearInterval(inter);
     }
 }
+
+var wrapper = document.querySelector('.logo svg')
+
+// We are only adding and removing the 'active' class,
+// the entire animation is defined in the CSS code
+function draw() {
+  wrapper.classList.add('active')
+}
+
+function erase() {
+  wrapper.classList.remove('active')
+}
+
+// Play draw animation once
+setTimeout(draw, 300)
